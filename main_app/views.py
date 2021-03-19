adea66a5338112766baa64ca8290a1b9009e20c0
aharri64 <haveacompleteday@gmail.com>
2021-03-19 16:35:10 -0600

    remanaged data

--- main_app/views.py
+++ main_app/views.py
@@ -5,4 +5,20 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1>Cat Collector</h1>')
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+# 1. Make a view function
+# 2. Make the html page
+# 3. Add the view to the urls.py inside of main_app.urls
+# In browser
+# When I go to localhost:8000/contact
+# Django -> urls -> /contact -> vews.contact (runs function) -> templates -> contact.html
